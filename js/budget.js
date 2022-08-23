1c451ce8bef54edf3e4d6691a8e0162b6cd69ec6
mrseeker53 <mi.nayeem30@gmail.com>
2022-08-23 21:46:09 +0600

    updated budget

--- js/budget.js
+++ js/budget.js
@@ -1,6 +1,14 @@
 // Player Expenses
-document.getElementById('btn-calculate').addEventListener('click', function () {
+const playerExpenses = function (event, param) {
+
+}
+document.getElementById('btn-calculate').addEventListener('click', function (event, param) {
     const perPlayerCost = getInputFieldValueById('player-cost');
+
+    // Input field takes only number otherwise return an alert message
+    if (isNaN(perPlayerCost)) {
+        return alert("Enter number for per player")
+    }
     let selected = 0;
     selected = arrayLength(selected);
 
@@ -12,15 +20,28 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     const textElementValue = parseFloat(textElementValueString);
     const newValue = textElementValue + playerExpenses;
     textElement.innerText = newValue;
+
+
+    // Total Expenses
+    document.getElementById('btn-calculate-total').addEventListener('click', function () {
+        const managerCost = getInputFieldValueById('manager-cost');
+        // Input field takes only number otherwise return an alert message
+        if (isNaN(managerCost)) {
+            return alert("Enter number for manager")
+        }
+        const coachCost = getInputFieldValueById('coach-cost');
+        // Input field takes only number otherwise return an alert message
+        if (isNaN(coachCost)) {
+            return alert("Enter number for coach")
+        }
+
+        const totalExpenses = playerExpenses + managerCost + coachCost;
+        console.log(totalExpenses)
+        const textElement = document.getElementById('total-cost');
+        const textElementValueString = textElement.innerText;
+        const textElementValue = parseFloat(textElementValueString);
+        const newValue = textElementValue + totalExpenses;
+        textElement.innerText = newValue;
+    })
 })
 
-// Total Expenses
-document.getElementById('btn-calculate-total').addEventListener('click', function () {
-    const managerCost = getInputFieldValueById('manager-cost');
-    const coachCost = getInputFieldValueById('coach-cost');
-
-    const playerExpenses = expenses(totalPlayerCost);
-    const totalExpenses = playerExpenses + managerCost + coachCost;
-    setTextElementValueById('total-cost', totalExpenses);
-    console.log(totalCost);
-})
